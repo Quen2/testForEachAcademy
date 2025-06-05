d1f7e28ad72d5a811b65b15079bf93080a0f442e
QuentinG <ZexFrost1@gmail.com>
2025-06-05 16:50:24 +0200

    [+] add css animation

--- test-foreach/components/sentences/sentence.js
+++ test-foreach/components/sentences/sentence.js
@@ -1,24 +1,38 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import GenerateSentence from "./generate-sentence"
 
 export default function Sentence () {
     
     const [sentence, SetSentence] = useState('')
+    const [showBtn, setShowBtn] = useState(false)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShowBtn(true)
+        }, 2000);
+    },[])
 
     return (
         <div className="w-screen h-screen bg-[#181818]">
             <div className="flex h-full flex-col">
-                <p className="text-center text-white text-4xl font-bold pt-4 h-1/3">ItWorksOnMyMachine</p>
-                <div className="h-2/3 text-center ">
-                    {
-                        sentence === 'loading' ?
-                        <p className="font-bold text-white text-4xl">Chargement..</p>
-                        :
-                        <p className="font-bold text-white text-4xl">{sentence}</p>
-                    }
-                    <GenerateSentence SetSentence={SetSentence} />
-                </div>
+                <div className="h-1/3"></div>
+                <p className="text-center absolute text-white text-4xl font-bold mx-auto w-full mainTitle">ItWorksOnMyMachine</p>
+                {
+                    showBtn 
+                    ?
+                    <div className="text-center generateSentence">
+                        {
+                            sentence === 'loading' ?
+                            <p className="font-bold text-white text-4xl">Chargement..</p>
+                            :
+                            <p className="font-bold text-white text-4xl">{sentence}</p>
+                        }
+                        <GenerateSentence SetSentence={SetSentence} />
+                    </div>
+                    :
+                    <></>
+                }
             </div>
         </div>
     )

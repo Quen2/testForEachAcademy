97e3dbb2a03df73173e6887b4eee49353b82c972
QuentinG <ZexFrost1@gmail.com>
2025-06-05 10:34:17 +0200

    [+] Project start

---
+++ test-foreach/components/sentences/generate-sentence.js
@@ -0,0 +1,7 @@
+export default function GenerateSentence () {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ test-foreach/components/sentences/sentence.js
@@ -0,0 +1,7 @@
+export default function Sentence () {
+    return (
+        <div>
+            
+        </div>
+    )
+}

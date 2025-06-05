3a8ca4a30718bed49f7373b4b7b225c7ccf41e11
QuentinG <ZexFrost1@gmail.com>
2025-06-05 17:12:36 +0200

    [~] fix apos

--- test-foreach/app/lost/page.js
+++ test-foreach/app/lost/page.js
@@ -4,7 +4,7 @@ export default function Lost () {
     return (
         <div className="w-screen h-screen bg-[#181818] flex flex-col">
             <div className="h-3/4 w-3/4 m-auto text-center">
-                <p className="text-white text-2xl font-bold">I'm lost...</p>
+                <p className="text-white text-2xl font-bold">I&apos;m lost...</p>
                 <img src={Gif.src} alt="cat gif" className="w-1/3 mx-auto"></img>
             </div>
         </div>
